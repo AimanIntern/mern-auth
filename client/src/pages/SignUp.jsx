c5856e37c01c7eeea1fc7920d731ca2dcf4b20b8
Aiman rasool <aimal.rasool2@internee.itskillxpress.com>
2024-02-22 15:20:58 +0500

    complete sigin page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate =useNavigate();
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value});
   };
@@ -21,12 +22,13 @@ export default function SignUp() {
       body: JSON.stringify(formData),
     });
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
     setLoading(false);
     if(data.success === false){
       setError(true);
       return;
     }
+    navigate('/sign-in');
       }catch(error){
     setLoading(false);
     setError(true);
@@ -35,9 +37,9 @@ export default function SignUp() {
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
     <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-      <input type='text' placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
+    <input type='username' placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg'onChange={handleChange}/>
       <input type='email' placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg'onChange={handleChange}/>
       <input type='password' placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg'onChange={handleChange}/>
       <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...': 'Sign Up'}</button>
