1fc93668cf4223209b65f7b423c24315570d2f8e
Aiman rasool <aimal.rasool2@internee.itskillxpress.com>
2024-02-27 19:25:57 +0500

    profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 export default function App() {
 return(
     <BrowserRouter >
@@ -13,7 +14,9 @@ return(
     <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/about" element={<About/>} />
+    <Route element={<PrivateRoute/>}>
     <Route path="/profile" element={<Profile/>} />
+    </Route>
     <Route path="/sign-in" element={<SignIn/>} />
     <Route path="/sign-up" element={<SignUp/>} />
     </Routes>
